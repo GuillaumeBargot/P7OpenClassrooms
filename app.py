c477b889c7e6ab49a8c78ac844d209c89f884990
GuillaumeBargot <guillaume.bargot@gmail.com>
2022-08-18 10:24:42 +0200

    Test getting the csv from the internet

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ app = FastAPI()
 
 def get_clean_data():
     clean_datas = []
-    for i in range(1,10):
-        clean_datas.append(pd.read_csv('notebooks/clean_data' + str(i) + '.csv'))
-        logging.warning("PDCONCAT" + clean_datas[len(clean_datas)-1].columns)
-    return pd.concat(clean_datas, ignore_index=True)
+    #for i in range(1,10):
+     #   clean_datas.append(pd.read_csv('notebooks/clean_data' + str(i) + '.csv'))
+      #  logging.warning("PDCONCAT" + clean_datas[len(clean_datas)-1].columns)
+    #return pd.concat(clean_datas, ignore_index=True)
+    return pd.read_csv('https://github.com/GuillaumeBargot/P7OpenClassrooms/blob/main/notebooks/clean_data.csv')
 
 model = joblib.load('model.joblib')
 clean_data = get_clean_data()
